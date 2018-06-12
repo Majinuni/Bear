935db842827a844f7765541ee3c8d82838c93bf3
Paul-weqe <paul1tw1@gmail.com>
2018-06-12 15:28:48 +0300

    Django setup

---
+++ django_majinuni/mainApp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.postgres.operations import CreateExtension
+
+# Create your models here.
+
+class Migration(migrations.Migration):
+    
+    operations = [
+        CreateExtension('postgis'),
+    ]
+
