5610bde096e70660afa1b33bdc12cfe9f940279b
Samwel Opiyo <samonyango20@gmail.com>
2018-06-12 16:05:56 +0300

    Add .gitignore, .dockerignore and use django-environ to get config parameters

--- django_majinuni/django_majinuni/settings.py
+++ django_majinuni/django_majinuni/settings.py
@@ -11,33 +11,73 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+ROOT_DIR = environ.Path(
+    __file__
+) - 1  # (django_majinuni/settings.py - 3 = django_majinuni/)
+
+# Load operating system environment variables and then prepare to use them
+env = environ.Env()
+
+# .env file, should load only in development environment
+READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=False)
+
+if READ_DOT_ENV_FILE:
+    # Operating System Environment variables have precedence over variables defined in the .env file,
+    # that is to say variables from the .env files will only be used if not defined
+    # as environment variables.
+    env_file = str(ROOT_DIR.path(".env"))
+    print("Loading : {}".format(env_file))
+    env.read_env(env_file)
+    print("The .env file has been loaded. See base.py for more information")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')te%k(9a0apo#2%-=^fpddlcammxw6(-hm2yuwi7bqn+pay57z'
+SECRET_KEY = env("DJANGO_SECRET_KEY", default=')te%k(9a0apo#2%-=^fpddlcammxw6(-hm2yuwi7bqn+pay57z')
+
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["*"])
+
+# Always use IPython for shell_plus
+SHELL_PLUS = "ipython"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = env.bool("DJANGO_DEBUG", default=True)
 
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+# APP CONFIGURATION
+# ------------------------------------------------------------------------------
+DJANGO_APPS = [
+    # Default Django apps:
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.gis",
+    # Admin
+    "django.contrib.admin",
 ]
+THIRD_PARTY_APPS = [
+
+]
+
+# Apps specific for this project go here.
+LOCAL_APPS = [
+
+]
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -67,6 +107,28 @@ TEMPLATES = [
     },
 ]
 
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "debug": DEBUG,
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.media",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+            ],
+        },
+    }
+]
+
+
 WSGI_APPLICATION = 'django_majinuni.wsgi.application'
 
 
@@ -74,16 +136,20 @@ WSGI_APPLICATION = 'django_majinuni.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'majinuni',
-        'USER': 'majinuni_user',
-        'PASSWORD': 'majinuni',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+    "default": {}
 }
+
+# 'ENGINE': 'django.contrib.gis.db.backends.postgis'
+# 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+DATABASES["default"] = env.db(
+    "DATABASE_URL",
+    default="postgis://majinuni_user:majinuni@localhost:5432/majinuni",
+)
+
+DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
+
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 
 # Password validation
