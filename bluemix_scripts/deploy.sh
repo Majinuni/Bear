14f7b95e871a2eda5f4a174e538b2fb777a1ec10
Samwel Opiyo <samonyango20@gmail.com>
2018-06-13 15:18:34 +0300

    Create postgis pod and postgis service

--- bluemix_scripts/deploy.sh
+++ bluemix_scripts/deploy.sh
@@ -19,10 +19,14 @@ echo "Build Docker Image for Application"
 bx cr build . --tag registry.ng.bluemix.net/test_api_registry_namespace/ibm_water_hackathon_bear:0.1.0
 
 echo -e "Creating pods"
-kubectl create -f kubernetes/deployments/python.yml
+kubectl apply -f kubernetes/deployments/python.yml
+kubectl apply -f kubernetes/deployments/postgis.yml
 
 echo -e "Creating Load Balancer"
-kubectl create -f kubernetes/services/python.yml
+kubectl apply -f kubernetes/services/python.yml
+kubectl apply -f kubernetes/services/postgis.yml
+
+kubectl get pods
 
 PORT=$(kubectl get services | grep frontend | sed 's/.*:\([0-9]*\).*/\1/g')
 
