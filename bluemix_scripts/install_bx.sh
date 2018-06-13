cfcf1e3ec9d5ae4fa0bf830ad7052fcd64cc3a2c
Samwel Opiyo <samonyango20@gmail.com>
2018-06-13 12:34:02 +0300

    Removed redundant install of ibm container service

--- bluemix_scripts/install_bx.sh
+++ bluemix_scripts/install_bx.sh
@@ -3,10 +3,6 @@
 echo "Download Bluemix CLI"
 curl -sL https://ibm.biz/idt-installer | bash
 
-# Install Armada CS plugin
-echo "Install the Bluemix container-service plugin"
-bluemix plugin install container-service -r Bluemix
-
 echo "Install kubectl"
 wget --quiet --output-document=/tmp/Bluemix_CLI/bin/kubectl  https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x /tmp/Bluemix_CLI/bin/kubectl
