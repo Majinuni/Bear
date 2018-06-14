456eda5a3f709a09ecfda4a0da6d1c41e0875bd4
Samwel Opiyo <samonyango20@gmail.com>
2018-06-14 11:17:12 +0300

    Updated templates and static files

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -1,14 +1,21 @@
-from django.urls import path
-from mainApp import views
+from django.conf.urls import url
+from .views import *
+
 
 urlpatterns = [
-    path("home/", views.home, name="name"),
-    # url(r'^home/$', home, name='home'),
-    path("", views.dashboard, name="dashboard"),
-    # url(r'^$', dashboard, name='dashboard'),
-    path("area/", views.area, name="area"),
-    # url(r'^area/$', area, name='area'),
-    path("water/", views.water, name="water"),
-    # url(r'^water/$', water, name='water'),
-    path("waterpoints/<str:county>", views.waterpoints, name="waterpoints"),
+	url(r'^home/$', home, name='home'),
+	#url(r'^$', dashboard, name='dashboard'),
+	url(r'^area/$', area, name='area'),
+	url(r'^water/$', water, name='water'),
+	url(r'^watermap/$', watermap, name='watermap'),
+	url(r'^$', dashboard, name='dashboard'),
+	url(r'^analysis/$', analysis, name='analysis'),
+	url(r'^predictor/$', predictor, name='predictor'),
+
+	
+
+
+
+
+
 ]
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import render
-from mainApp.fetch_json import get_waterpoints
-from django.http import JsonResponse
+
 
 # Create your views here.
 def home(request):
-    return render(request, "mainapp/base.html", {})
+    	return render(request, 'mainapp/base.html', {})
 
-
-def dashboard(request):
-    return render(request, "mainapp/dashboard.html", {})
+#def dashboard(request):
+    	#return render(request, 'mainapp/dashboard.html', {})
 
 
 def area(request):
-    return render(request, "mainapp/area.html", {})
-
+    	return render(request, 'mainapp/area.html', {})
 
 def water(request):
-    return render(request, "mainapp/water.html", {})
+    	return render(request, 'mainapp/water.html', {})
 
+def watermap(request):
+    	return render(request, 'mainapp/watermap.html', {})
 
-def waterpoints(request, county):
-    marsabit_waterpoints = get_waterpoints(county=county)
-    return JsonResponse(marsabit_waterpoints)
+def dashboard(request):
+    	return render(request, 'mainapp/reports.html', {})
+
+def analysis(request):
+    	return render(request, 'mainapp/analysis.html', {})
+
+def predictor(request):
+    	return render(request, 'mainapp/predictor.html', {})
